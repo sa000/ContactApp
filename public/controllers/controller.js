d18bf971b3bbaf078904de7d0639fdeed542c6ce
sa000 <alamsakib@gmail.com>
2015-11-10 02:11:22 -0600

    commits
    Can filter by professor/name, and resolved year/semester issues.

---
+++ public/controllers/controller.js
@@ -0,0 +1,94 @@
+var myApp = angular.module('myApp', []);
+
+
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+
+    $scope.semester= ["Spring","Summer","Fall"];
+    $scope.semesterIndex=0;
+
+    console.log("Hello World from controller");
+
+
+
+    $http.get('/overallCount').success(function(response) {
+          console.log("I got the count of overall review count of 5");
+          $scope.overall5 = response.FiveStarCount;
+          $scope.overall4 = response.FourStarCount;
+          $scope.overall3 = response.ThreeStarCount;
+          console.log("I think 3 star responses work"+response.ThreeStarCount)
+          $scope.overall2 = response.TwoStarCount;
+          $scope.overall1 = response.OneStarCount;
+          $scope.overallTotal=$scope.overall1+2*$scope.overall2+3*$scope.overall3+4*$scope.overall4+5*$scope.overall5;
+                });
+
+
+
+
+
+
+        this.addReview = function (reviews) {
+            this.review.createdOn = Date.now();
+            this.reviews.push(this.review);
+            this.review = {};
+          };
+
+$http.get('/contactlist').success(function($scope, response) {
+  console.log("I got the DATA")
+  $scope.contactlist = response;
+  $scope.contact = ""; // clears out input box
+});
+// insert review count, controller talks to server through url (routes, name does not matter jsut describe what it does
+// if success contact server comes back here
+$http.get('/contactlistcount').success(function(response) {
+// should have console.log to make sure sent
+console.log("THIS SHIZ WORKS AND HAS BEEN RETREIVED");
+// my parameter is response so since received that to scope to send back through reviewCount within brackets under ng-controller
+$scope.reviewCount = response;
+console.log("I think responses work"+response)
+// !!!!!!! DOES NOT REFRESH LIVE FIX LATER
+});
+
+$scope.filter = function (crit1) {
+	console.log("The filtered button has been pressed");
+  console.log("The  filtered number is "+crit1)
+  $http.get('/filter/'+crit1).success(function(response){
+    console.log("received the filtered reviews"+response)
+    $scope.contactlist=response;
+ });
+
+};
+
+
+$scope.addContact = function () {
+  $scope.contact["semester"]=  $scope.semester[$scope.contact["semesterIndex"]]
+  $scope.contact["year"]=parseInt($scope.contact["year"])
+	console.log("Your review is"+$scope.contact);
+	$http.post('contactlist', $scope.contact).success(function(response) {
+		console.log("response:"+response);
+ 	});
+};
+
+$scope.remove = function(id) {
+	console.log(id);
+	$http.delete('/contactlist/' + id).success(function(response) {
+ 	});
+};
+
+$scope.edit = function(id) {
+	console.log(id);
+	$http.get('/contactlist/' + id).success(function(response) {
+		$scope.contact = response;
+	});
+};
+
+$scope.update = function() {
+	console.log($scope.contact._id);
+	$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+ 	})
+};
+
+$scope.deselect = function() {
+	$scope.contact = "";
+}
+
+}]);
---
+++ server.js
@@ -0,0 +1,168 @@
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+//var dburl='localhost/contactlist'
+//var collections=['contactlist']
+//var mongojs = require('mongojs');
+
+var bodyParser = require('body-parser');
+var util = require('util');
+//var url='mongodb://localhost:27017/contactlist';
+//var MongoClient = require('mongodb').MongoClient;
+
+
+
+
+
+var numberreviews=function overallCountClass(){
+
+}
+
+var overallCount=new numberreviews();
+
+// NEW STUFF ENDS
+
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+app.get('/contactlist', function (req, res) {
+	console.log("I received a GET request")
+
+	db.contactlist.find(function (err, docs) {
+		console.log(docs);
+		res.json(docs);
+	});
+});
+
+
+app.post('/contactlist', function (req, res) {
+	req.body._id=0;
+	db.contactlist.insert(req.body, function(err, doc) {
+		res.json(doc);
+	});
+});
+
+app.delete('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+		res.json(doc);
+	});
+});
+
+app.get('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+		res.json(doc);
+	});
+});
+
+app.put('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(req.body.name);
+	db.contactlist.findAndModify({
+		query: {_id: mongojs.ObjectId(id)},
+		update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+		new: true}, function (err, doc) {
+			res.json(doc);
+		}
+	);
+});
+
+
+
+// got request and response, if want mongo us db."nameofcollection"."function", then use "err, docs"
+app.get('/contactlistcount', function (req, res) {
+	console.log("THIS HAS BEEN RECEIVED");
+	db.contactlist.count(function(err,docs) {
+ 		console.log(docs);
+ 		res.json(docs);
+	});
+});
+
+
+// testing stars rating
+/*
+app.get('/overallcount', function (req, res) {
+	console.log("I received an overall get request");
+	var overall5 = db.contactlist.find({overallrating:5}).count();
+	var overall4 = db.contactlist.find({overallrating:4}).count();
+	response = [overall4, overall5]
+	res.json(response);
+
+});
+*/
+
+// NEW STUFF 11-3-15
+/*
+use overallCount so easier to debug
+
+FOR ROGER: CHANGE NAME OF DATABASE TO MATCH UP
+*/
+app.get('/overallCount', function (req, res){//sends this to the controller from $http.get which is the request
+console.log("I received a get request for the overallacount");
+
+db.contactlist.find({overallrating:5}).count(function(err, doc) {
+  overallCount.FiveStarCount=doc;
+	console.log("I received a 5 star request for the overallacount"+doc);
+
+});
+
+db.contactlist.find({overallrating:4}).count(function(err, doc) {
+  overallCount.FourStarCount=doc;
+	console.log("I received a 4 star request for the overallacount"+doc);
+
+});
+db.contactlist.find({overallrating:3}).count(function(err, doc) {
+  overallCount.ThreeStarCount=doc;
+  console.log("I received a 3 star request for the overallacount"+doc);
+});
+console.log("transition between 3 and 2");
+db.contactlist.find({overallrating:2}).count(function(err, doc) {
+  overallCount.TwoStarCount=doc;
+	console.log("I received a 2 star request for the overallacount"+doc);
+
+});
+console.log("transition between 2 and 1");
+db.contactlist.find({overallrating:1}).count(function(err, doc) {
+  overallCount.OneStarCount=doc;
+	res.json(overallCount);
+
+	console.log("I received a 1 star request for the overallacount"+doc);
+
+});
+console.log("I have all my reviews ready to go");
+
+
+
+//classes should be capitlized, functions should be capitlized, everything else is camel case
+ //db.reviewlist.find({overallrating:4}).count(function(err, doc) {
+ //   res.json(doc);
+  //});
+  //db.reviewlist.find({overallrating:3}).count(function(err, doc) {
+  //   res.json(doc);
+   //});
+ });
+
+ app.get('/filter/:crit1', function (req, res) {
+ 	console.log("I received a FILTERED request")
+ 	var crit1=req.params.crit1
+
+ 	console.log("did the criteria work?"+crit1)
+ 	db.contactlist.find({ $or: [ {professor:crit1}, {course:crit1} ] } ,function (err, docs) {
+
+ 		console.log("Filtered reviews are below"+docs);
+ 		res.json(docs);
+ 	});
+
+ });
+
+// NEW STUFF ENDS
+//});
+
+
+app.listen(3000);
+console.log("server running on port 3000");
